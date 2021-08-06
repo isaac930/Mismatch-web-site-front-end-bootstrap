f2542259116f93b1e3d8dad5fd6fb96aa72f7b4c
isaac930 <kirumiraisaacsoft@gmail.com>
2021-08-06 20:19:28 +0300

    release mismatch front changes

--- chat.php
+++ chat.php
@@ -3,24 +3,19 @@ include 'connection.php';
 $email = $_GET['email'];
 $my_email = $_GET['my_email'];
 
-//$query = "SELECT * FROM chats where ((email='$email' and chatment_email='$my_email') or (email='$email' and chatment_email='')) or ((email='$my_email' and chatment_email='$email') or (email='$my_email' and chatment_email=''))   ORDER BY id DESC";
-
 $query = pg_query($con, "SELECT * FROM chats where ((email='$email' and chatment_email='$my_email') or (email='$email' and chatment_email='')) or ((email='$my_email' and chatment_email='$email') or (email='$my_email' and chatment_email=''))   ORDER BY id DESC");
-
-//$run = $con -> query($query);
-//while ($row = $run->fetch_array()) :
 
 while ($row = pg_fetch_row($query)) :
 ?>
 <div class="alert alert-danger" role="alert">
 <?php $baseurl = "https://isaacbucketenock.s3.us-east-2.amazonaws.com/users/"; ?>
-<img src="<?php echo $baseurl.$row['image_path']?>" width="70" height="70" class="rounded-circle float-left" alt="profile-picture" border="0" />
+<img src="<?php echo $baseurl.$row[9]?>" width="70" height="70" class="rounded-circle float-left" alt="profile-picture" border="0" />
 <br/>
 <br/>
-<a class="message-author" href="#"> <?php echo $row['name'];?> </a>
+<a class="message-author" href="#"> <?php echo $row[2];?> </a>
 <hr>
-<span class="message-date"> <?php echo $row['date'];?> </span>
+<span class="message-date"> <?php echo $row[1];?> </span>
 <hr>
-<span class="message-content"> <?php echo $row['post'];?> </span>
+<span class="message-content"> <?php echo $row[8];?> </span>
 </div>
 <?php endwhile; ?>
