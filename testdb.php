b2b0f64b4c7b7312a1e3f8db64d7499d17580dba
isaac930 <kirumiraisaacsoft@gmail.com>
2021-08-06 20:14:19 +0300

    release mismatch front changes

--- testdb.php
+++ testdb.php
@@ -8,10 +8,10 @@ $query = pg_query($con, "SELECT * FROM chats ORDER BY id DESC");
 while ($row = pg_fetch_row($query)) :
 ?>
 <div class="alert alert-danger" role="alert">
-<a class="message-author" href="#"> <?php echo $row['name'];?> </a>
+<a class="message-author" href="#"> <?php echo $row[0];?> </a>
 <hr>
-<span class="message-date"> <?php echo $row['date'];?> </span>
+<span class="message-date"> <?php echo $row[1];?> </span>
 <hr>
-<span class="message-content"> <?php echo $row['post'];?> </span>
+<span class="message-content"> <?php echo $row[2];?> </span>
 </div>
 <?php endwhile; ?>
