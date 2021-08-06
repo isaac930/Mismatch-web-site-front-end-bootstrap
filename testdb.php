1d9e41dae0e7f0195f49251c6b9c684a2fa131b0
isaac930 <kirumiraisaacsoft@gmail.com>
2021-08-06 20:11:54 +0300

    release mismatch front changes

--- testdb.php
+++ testdb.php
@@ -8,10 +8,6 @@ $query = pg_query($con, "SELECT * FROM chats ORDER BY id DESC");
 while ($row = pg_fetch_row($query)) :
 ?>
 <div class="alert alert-danger" role="alert">
-<?php $baseurl = "https://isaacbucketenock.s3.us-east-2.amazonaws.com/users/"; ?>
-<img src="<?php echo $baseurl.$row['image_path']?>" width="70" height="70" class="rounded-circle float-left" alt="profile-picture" border="0" />
-<br/>
-<br/>
 <a class="message-author" href="#"> <?php echo $row['name'];?> </a>
 <hr>
 <span class="message-date"> <?php echo $row['date'];?> </span>
